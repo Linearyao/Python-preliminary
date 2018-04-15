1a7593e6d096bba461dde4c8e3684549e499f9c3
Linearyao <read707@sina.com>
2018-04-15 18:56:16 +0800

    Delete ch1_12.py

--- Chapter 1/ch1_12.py
+++
@@ -1,5 +0,0 @@
-import nltk
-from nltk.tokenize import RegexpTokenizer
-sent=" She secured 90.56 % in class X . She is a meritorious student"
-capt = RegexpTokenizer('[A-Z]\w+')
-print(capt.tokenize(sent))
