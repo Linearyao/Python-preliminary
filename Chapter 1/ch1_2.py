a3eabef8ea8a37448b406ab63c986028aa125b25
Linearyao <read707@sina.com>
2018-04-15 18:57:30 +0800

    Delete ch1_2.py

--- Chapter 1/ch1_2.py
+++
@@ -1,4 +0,0 @@
-import nltk
-tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
-text=" Hello everyone. Hope all are fine and doing well. Hope you find the book interesting"
-print(tokenizer.tokenize(text))
