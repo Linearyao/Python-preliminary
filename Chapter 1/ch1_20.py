85d36a896789331b1a2261d8df2b44b0ac39b313
Linearyao <read707@sina.com>
2018-04-17 09:57:05 +0800

    Delete ch1_20.py

--- Chapter 1/ch1_20.py
+++
@@ -1,4 +0,0 @@
-import nltk
-from nltk.tokenize.util import string_span_tokenize
-sent=" She secured 90.56 % in class X \n. She is a meritorious student\n"
-print(list(string_span_tokenize(sent, " ")))
