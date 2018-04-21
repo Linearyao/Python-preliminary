04a99f5f01aecce7ca5ce3dd9f2b568ddc0973b3
Linearyao <read707@sina.com>
2018-04-21 20:48:42 +0800

    Delete ch1_21.py

--- Chapter 1/ch1_21.py
+++
@@ -1,4 +0,0 @@
-text=[" It is a pleasant evening.","Guests, who came from US arrived at the venue","Food was tasty."]
-from nltk.tokenize import word_tokenize
-tokenized_docs=[word_tokenize(doc) for doc in text]
-print(tokenized_docs)
